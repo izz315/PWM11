aafc9df8b1794e254fa2e79c9d3f82a75893bb39
izz315 <154888989+izz315@users.noreply.github.com>
2025-06-11 14:18:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
+// Event 'install' akan dijalankan saat service worker pertama kali dipasang
 self.addEventListener("install", event => {
   event.waitUntil(
+    // Membuka cache bernama "pwa-assets"
     caches.open("pwa-assets").then(cache => {
+      // Menambahkan file-file penting ke dalam cache
       return cache.addAll([
-        "./", 
-        "./index.html", 
-        "./app.js", 
-        "./manifest.json",
-        "./icons8-instagram-50.png", 
-        "./icons8-linkedin-50.png",
-        "./icons8-whatsapp-50.png", 
-        "./icons8-x-50.png", 
-        "./izz.jpg", 
-        "./apkicon.jpg"
+        "./",                    // root
+        "./index.html",         // halaman utama
+        "./app.js",             // file JavaScript utama
+        "./manifest.json",      // manifest PWA
+        "./icons8-instagram-50.png", // ikon Instagram
+        "./icons8-linkedin-50.png",  // ikon LinkedIn
+        "./icons8-whatsapp-50.png",  // ikon WhatsApp
+        "./icons8-x-50.png",         // ikon X/Twitter
+        "./izz.jpg",            // foto profil
+        "./apkicon.jpg"         // ikon aplikasi
       ]);
     })
   );
 });
 
+// Event 'fetch' akan menangani setiap permintaan sumber daya (asset)
 self.addEventListener("fetch", event => {
   event.respondWith(
+    // Mencoba mencocokkan permintaan dengan cache
     caches.match(event.request).then(cachedResponse => {
+      // Jika ada di cache, kembalikan dari cache. Jika tidak, ambil dari jaringan.
       return cachedResponse || fetch(event.request);
     })
   );
