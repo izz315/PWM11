91a9b9f6dd86777239991268d8e579b6e12707c3
izz315 <154888989+izz315@users.noreply.github.com>
2025-06-11 13:47:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,25 +4,16 @@ registerSW();
 async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
-      // Change the service worker URL to a non-existent file (e.g., "nonexistent-sw.js")
-      // to test how the application handles errors when the service worker file is missing.
-      const swUrl = "sw.js";
-      await navigator.serviceWorker.register(swUrl);
-      showResult("Service worker registered successfully!");       
+      const registration = await navigator.serviceWorker.register("./sw.js");
+      console.log("Service worker registered", registration); // Tambahan
     } catch (error) {
-      showResult(`Error while registering: ${error.message}`);
       showResult("Error while registering: " + error.message);
     }
   } else {
-      showResult("Service workers API not available");
+    showResult("Service workers API not available");
   }
-}
+};
 
 function showResult(text) {
-  const outputElement = document.querySelector("output");
-  if (outputElement) {
-    outputElement.innerHTML = text;
-  } else {
-    console.error("Output element not found in the DOM.");
-  }
+  document.querySelector("output").innerHTML = text;
 }
